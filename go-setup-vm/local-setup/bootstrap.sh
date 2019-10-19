1af75824faeeaf887c305dcb25f2ed75688d3023
Vikram Jadhav <vikramcse.10@gmail.com>
2019-10-19 17:09:29 +0530

    added google keys for cloud sdk

--- go-setup-vm/local-setup/bootstrap.sh
+++ go-setup-vm/local-setup/bootstrap.sh
@@ -11,6 +11,10 @@ if [ "${UPGRADE_PACKAGES}" != "none" ]; then
 
   # Add third party repositories
   sudo add-apt-repository ppa:keithw/mosh-dev -y
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  apt-get install apt-transport-https ca-certificates
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  sudo apt-get update && sudo apt-get install google-cloud-sdk-engine-go
 
   CLOUD_SDK_SOURCE="/etc/apt/sources.list.d/google-cloud-sdk.list"
   CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
